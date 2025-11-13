ad583dbaeae448a99e456187f1fb99dae545cf04
DominicHuntBit <dominichj2006@gmail.com>
2025-11-13 13:04:29 -0500

    feat: registro implementado

--- Frontend/src/app/core/services/auth.service.ts
+++ Frontend/src/app/core/services/auth.service.ts
@@ -18,11 +18,25 @@ export class AuthService {
     );
   }
 
+  register(userData: {
+    nombre: string;
+    nombre_usuario: string;
+    email: string;
+    contrasena: string;
+    telefono?: string;
+    edad: string;
+    saldo_inicial: number;
+  }): Observable<any> {
+    return this.http.post<any>(
+      `${this.baseUrl}/auth/registro`,
+      userData
+    );
+  }
+
   storeTokens(tokens: { accessToken: string; refreshToken: string }): void {
     localStorage.setItem('accessToken', tokens.accessToken);
     localStorage.setItem('refreshToken', tokens.refreshToken);
   }
-
 
   getToken(): string | null {
     return localStorage.getItem('accessToken');
